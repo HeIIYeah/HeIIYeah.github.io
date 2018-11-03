9709c1dfd4af0e096036ada731ad4f5242a7f3fd
HeIIYeah <st_ajiex@inbox.ru>
2018-11-03 13:22:18 +0600

    Added sidebar at lesson 13.5

--- lesson_13.5/js/style.js
+++ lesson_13.5/js/style.js
@@ -39,32 +39,26 @@ $('.feautures-sidebar').slick({
 });
 
 $('.second-sidebar').slick({
- infinite: true,
- slidesToShow: 2,
- slidesToScroll: 2,
- responsive: [{
-  breakpoint: 10000,
-  settings: {
-   slidesToShow: 1,
-   slidesToScroll: 1,
    prevArrow: '<button class="prev arrow"></button>',
-   nextArrow: '<button class="next arrow"></button>'
-  }
+   nextArrow: '<button class="next arrow"></button>',
+   draggable: false
  }
- ]
-});
+ 
+);
 
 $('.slider-for').slick({
  arrows: false,
  fade: true,
  draggable: false,
+ dots: true,
  asNavFor: '.slider-nav'
 });
 $('.slider-nav').slick({
  slidesToShow: 3,
- slidesToScroll: 3,
+ slidesToScroll: 1,
  asNavFor: '.slider-for',
  centerMode: true,
  draggable: false,
+ arrows: false,
  focusOnSelect: true
 });
